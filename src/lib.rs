36dfb8c86ca12ba7c0fb937d5da186ae220059bb
Arthur Roberts <arfa.r@live.com>
2025-08-27 23:55:10 +0100

    Scaffolding done for some basic searching

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,13 @@ mod db;
 pub use db::{
     check_db_exists_and_populated, find_matching_cards, find_matching_cards_scryfall_style,
     get_all_card_names, get_all_lowercase_card_names, get_all_mtg_words, get_card_by_name, init_db,
-    update_db_with_file, DbCard, DbExistanceErrors, GetNameType,
+    percentage_search_strings, update_db_with_file, DbCard, DbExistanceErrors, GetNameType,
 };
 
 mod utils;
 pub use utils::{create_local_data_folder, get_local_data_folder, get_local_data_sqlite_file};
 
+#[derive(Debug)]
 pub enum CardMatchResult {
     DidYouMean(Vec<String>),
     MultipleCardsMatch(Vec<DbCard>),
@@ -35,7 +36,8 @@ pub fn find_magic_words_with_close_spelling(search_text: &Vec<String>) -> Vec<(u
 }
 
 pub fn try_match_card(search_text: &Vec<String>) -> CardMatchResult {
-    let mut matching_cards = find_matching_cards_scryfall_style(search_text);
+    let percentaged_search_text = percentage_search_strings(search_text);
+    let mut matching_cards = find_matching_cards_scryfall_style(&percentaged_search_text);
 
     if matching_cards.is_empty() {
         let close_names = find_magic_words_with_close_spelling(search_text);
