b08822cdb6d17aadee91427e09684160b55d3370
Arthur Roberts <arfa.r@live.com>
2025-08-28 00:45:44 +0100

    Figured out how to pass to stdin

--- src/mf_rofi.rs
+++ src/mf_rofi.rs
@@ -4,7 +4,8 @@ use magic_finder::CardMatchResult;
 use magic_finder::DbCard;
 use magic_finder::GetNameType;
 use std::io::ErrorKind;
-use std::process::Command;
+use std::io::Write;
+use std::process::{Command, Stdio};
 
 fn initial_rofi() -> String {
     let output = Command::new("rofi")
@@ -36,7 +37,23 @@ fn rofi_print_card(card: DbCard) {
         }
         None => card.to_string(),
     };
-    Command::new("rofi").args(["-e", &display_string]).output();
+    let _ = Command::new("rofi").args(["-e", &display_string]).output();
+}
+
+fn rofi_show_did_you_mean(magic_words: &Vec<String>) -> String {
+    let magic_words_as_one_string = magic_words.join("\n");
+    let mut child = Command::new("rofi")
+        .arg("-dmenu")
+        .arg("-i")
+        .args(["-p", "Did you mean"])
+        .stdin(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let child_stdin = child.stdin.as_mut().unwrap();
+    let _ = child_stdin.write_all(magic_words_as_one_string.as_bytes());
+    let output = child.wait_with_output().unwrap();
+    let output = String::from_utf8(output.stdout.clone()).unwrap();
+    output
 }
 
 fn main() {
@@ -54,7 +71,10 @@ fn main() {
     let card_search_result = try_match_card(&search_text_words);
     dbg!(&card_search_result);
     match card_search_result {
-        CardMatchResult::DidYouMean(magic_words) => {}
+        CardMatchResult::DidYouMean(magic_words) => {
+            let selected_word = rofi_show_did_you_mean(&magic_words);
+            println!("{}", selected_word);
+        }
         CardMatchResult::MultipleCardsMatch(cards) => {}
         CardMatchResult::ExactCardFound(card) => {
             rofi_print_card(card);
